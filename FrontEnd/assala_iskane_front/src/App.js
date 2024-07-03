2488a1cb1a072d64670d70dee90731b0a73a6984
YOUSSEF ASSOIL <assoilyoussef02@gmail.com>
2024-07-03 16:07:39 +0100

    FrontEndSetup !

---
+++ FrontEnd/assala_iskane_front/src/App.js
@@ -0,0 +1,13 @@
+import './CSS/App.css';
+import logo from "./images/logo00.png"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
