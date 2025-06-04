b6beeb49520b20c1be506a51639607b10bf3ce67
MOHAMEDREDABOUAMOUD <mohamedredabouamoud@gmail.com>
2025-06-04 20:58:53 +0100

    Conteneuriser le projet

--- FrontEnd/assala_iskane_front/src/Components/constants.js
+++
@@ -1,6 +0,0 @@
-export const getName = () => localStorage.getItem('NAME') || "Home";
-export const setName = (name) => localStorage.setItem('NAME', name);
-export const getUSERID = () => localStorage.getItem('USERID') || "U001";
-export const setUSERID = (id) => localStorage.setItem('USERID', id);
-export const getPROJECTID = () => localStorage.getItem('PROJECTID') || "P001";
-export const setPROJECTID = (id) => localStorage.setItem('PROJECTID', id);
